ba66f5dd533068a5e06a5508ceb2ae805281100a
Stephan Paul <stephanpauld@gmail.com>
2021-02-10 17:31:56 -0700

    Learning Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Player from './components/player'
+import m1 from './assets/sprites/m1.png'
+import m2 from './assets/sprites/m2.png'
+import e1 from './assets/sprites/e1.png'
+import f1 from './assets/sprites/f1.png'
+import f2 from './assets/sprites/f2.png'
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="zone-container">
+      <Player
+      skin={m2}
+      />
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/hooks/use-key-press/index.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react'
+
+export default function useKeyPress(fn) {
+
+
+  useEffect(() => {
+    window.addEventListener("keydown", fn);
+    return () => window.removeEventListener("keydown", fn);
+  }, [fn])
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
